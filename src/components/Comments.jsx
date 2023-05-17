5b6a0a2aec86949683342570634e1c5c3099ae6d
michal <michklak91@gmail.com>
2023-05-17 13:46:01 +0200

    trying style grid

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -13,24 +13,28 @@ export default function CommentsSection() {
 			name: 'Anisha Li',
 			comment:
 				"Manage has supercharged our team's workflow. The ability to maintain visbility on larger milestones at all times keeps everyone motivated",
+			key: 1,
 		},
 		{
 			img: '../../images/avatar-ali.png',
 			name: 'Ali Bravo',
 			comment:
 				'We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused',
+			key: 2,
 		},
 		{
 			img: '../../images/avatar-richard.png',
 			name: 'Richard Watts',
 			comment:
 				"Manage allows us to provide structure and process. It keeps us organized and focused. I can't stop recommending them to everyone I talk to",
+			key: 3,
 		},
 		{
 			img: '../../images/avatar-shanai.png',
 			name: 'Shanai Gough',
 			comment:
 				'Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive',
+			key: 4,
 		},
 	]
 
@@ -64,7 +68,8 @@ export default function CommentsSection() {
 			<h2>What they've said</h2>
 			<div className={(!isDesktop && 'commentsBox one') || 'commentsBox three'}>
 				{users.map(user => {
-					return <Card name={user.name} avatar={user.img} comment={user.comment} />
+					console.log(user)
+					return <Card name={user.name} avatar={user.img} comment={user.comment} key={user.key} />
 				})}
 			</div>
 			<button className='getStarted'>Get Started</button>
